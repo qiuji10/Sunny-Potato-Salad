4ad56f61cd1c4efad34be9c7c5ddae9ed28abda4
qiuji <74701224+qiuji10@users.noreply.github.com>
2023-07-29 13:04:49 +0800

    Timer and TMP

---
+++ Assets/0_Project/1_Scripts/Timer System/TimerEvent.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimerEvent : MonoBehaviour
+{
+    [SerializeField] private TMP_Text timerText;
+
+    void Awake()
+    {
+        Timer.OnTimerChange += Timer_OnTimerChange;
+    }
+
+    void OnDestroy()
+    {
+        Timer.OnTimerChange -= Timer_OnTimerChange;
+    }
+
+    private void Timer_OnTimerChange(float time)
+    {
+        timerText.text = Timer.FormatTime(time);
+    }
+}
